4c5855abc848fa74ef372d7c919ce93e329eeb6e
DirtCobleGrass <maneirogamerx@gmail.com>
2024-04-02 18:18:04 -0300

    alarm email added (not working)

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 function gerarHtmlAvisoPrecos(jsonData) {
   // Obtém os valores do JSON
-  const prata = jsonData.rates.XAG;
-  const ouro = jsonData.rates.XAU;
+  const prata = Number(jsonData.rates.XAG.toFixed(2));
+  const ouro = Number(jsonData.rates.XAU.toFixed(2));
   const altaPrata = jsonData.info.high_mean_diff_xag;
   const altaOuro = jsonData.info.high_mean_diff_xau;
 
@@ -37,7 +37,7 @@ function gerarHtmlAvisoPrecos(jsonData) {
   if (altaPrata) {
     html += `
       <div class="mensagem">
-        <p>Há indicação de alta nos preços da prata. Valor atual: ${prata}</p>
+        <p>Há indicação de <strong>variação alta</strong> nos preços da <strong>prata</strong>. Valor atual: R$<span style="color: red">${prata}</span></p>
       </div>
     `;
   }
@@ -46,16 +46,25 @@ function gerarHtmlAvisoPrecos(jsonData) {
   if (altaOuro) {
     html += `
       <div class="mensagem">
-        <p>Há indicação de alta nos preços do ouro. Valor atual: ${ouro}</p>
+        <p>Há indicação de <strong>variação alta</strong> nos preços do <strong>ouro</strong>. Valor atual: R$<span style="color: red">${ouro}</span></p>
       </div>
     `;
   }
 
   // Se não houver indicação de alta para prata ou ouro
-  if (!altaPrata && !altaOuro) {
+  if (!altaOuro) {
     html += `
       <div class="mensagem">
-        <p>Não há indicação de alta nos preços de prata ou ouro.</p>
+        <p><strong>Não</strong> há indicação de <strong>variação alta</strong> nos preços da <strong>prata</strong>. Valor atual: R$<span style="color: green">${prata}</span></p>
+      </div>
+    `;
+  }
+
+  // Se não houver indicação de alta para prata ou ouro
+  if (!altaPrata) {
+    html += `
+      <div class="mensagem">
+        <p><strong>Não</strong> há indicação de <strong>variação alta</strong> nos preços do <strong>ouro</strong>. Valor atual: R$<span style="color: green">${ouro}</span></p>
       </div>
     `;
   }
@@ -69,6 +78,9 @@ function gerarHtmlAvisoPrecos(jsonData) {
   return html;
 }
 
+
+
+
 // Exemplo de uso
 const jsonData = {
   "success": true,
@@ -81,7 +93,7 @@ const jsonData = {
   "info": {
     "mean_diff_xag": -0.0011137635232325524,
     "mean_diff_xau": -0.004249402376749703,
-    "high_mean_diff_xau": false,
+    "high_mean_diff_xau": true,
     "high_mean_diff_xag": true
   },
   "expirationTimestamp": 1712082796833
@@ -89,3 +101,31 @@ const jsonData = {
 
 const htmlAvisoPrecos = gerarHtmlAvisoPrecos(jsonData);
 console.log(htmlAvisoPrecos);
+
+
+export async function sendEmail(recipient, subject, html_content) {
+  const sendGridSecret = JSON.parse(await wixSecretsBackend.getSecret('SendGridSecret'));
+  const key = sendGridSecret.key;
+  const senderEmail = sendGridSecret.senderEmail;
+
+  sgMail.setApiKey(key);
+
+  const msg = {
+      to: recipient,
+      from: senderEmail,
+      subject: subject,
+      html: html_content,
+  }
+
+  try {
+      const response = await sgMail.send(msg);
+      return [response[0].statusCode, response[0].headers];
+  } catch (error) {
+      console.error(error);
+  }
+}
+
+// Exemplo de uso
+const recipient = 'mayara@ybybank.comm.br'
+const subject = 'Indicação de Alta de Preços';
+const html_content = htmlAvisoPrecos;
