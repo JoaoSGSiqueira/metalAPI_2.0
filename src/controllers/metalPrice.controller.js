85580fd00d78dcdddb7f3fc605dfc4ef613ca225
DirtCobleGrass <maneirogamerx@gmail.com>
2024-09-05 17:56:59 -0300

    removed function for it to work

--- src/controllers/metalPrice.controller.js
+++ src/controllers/metalPrice.controller.js
@@ -7,7 +7,6 @@ import {
 } from "../services/metalPrice.service.js";
 
 dotenv.config();
-const TIMES_TO_UPDATE = process.env.TIMES_TO_UPDATE ? process.env.TIMES_TO_UPDATE.split(',') : [];
 
 export const getMetalPrice = async (req, res, next) => {
   try {
@@ -22,17 +21,6 @@ export const getAllMetalPrices = async (req, res, next) => {
   try {
     const allData = await getDbData();
     return res.json(allData);
-  } catch (error) {
-    next(error);
-  }
-}
-
-
-export const getClosestMetalPrice = async (req, res, next) => {
-  try {
-    const allData = await getDbData();
-    const closestData = await getClosestMetalPriceData(TIMES_TO_UPDATE, allData);
-    return res.json(closestData);
   } catch (error) {
     next(error);
   }
